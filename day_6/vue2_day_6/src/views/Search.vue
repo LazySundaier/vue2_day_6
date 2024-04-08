539a58637cb91375ce51f144d4aee18f65b73d6d
2795402901@qq.com <2795402901@qq.com>
2024-04-08 16:48:48 +0800

    查询参数传参

---
+++ day_6/vue2_day_6/src/views/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search">
+    <!-- 用同名参数接受 -->
+    <p>搜索关键字: {{ $route.query.key }} </p>
+    <p>搜索结果: </p>
+    <ul>
+      <li>.............</li>
+      <li>.............</li>
+      <li>.............</li>
+      <li>.............</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyFriend',
+  created () {
+    // 在created中，获取路由参数,要加this
+    // this.$route.query.参数名 获取
+    console.log(this.$route.query.key);
+  }
+}
+</script>
+
+<style>
+.search {
+  width: 400px;
+  height: 240px;
+  padding: 0 20px;
+  margin: 0 auto;
+  border: 2px solid #c4c7ce;
+  border-radius: 5px;
+}
+</style>
